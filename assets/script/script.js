0a02f9be4f45803b2c8af35c33d20d41b7a8cec0
Esra Al <esra.wameed@yahoo.com>
2022-09-23 21:59:19 -0400

    added descriptive comments to script.js file

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,23 +1,24 @@
+//added a live timer through SetInterval and moment.js to be displayed in the header section
 setInterval(() => $("#currentDay").text(moment(new Date().toLocaleString()).format("MMM Do, YYYY, hh:mm:ss")), 1000);
-timeColorChange
+//added a function to take in compare live time from moment.js to time in class id and color code by adding classes based on our if/else conditions
 function timeColorChange() {
     $(".time-block").each(function() {
-        var currHour = parseInt($(this).attr("id"));
-        if (currHour > moment().hours()) {
+        if (parseInt($(this).attr("id")) > moment().hours()) {
             $(this).addClass("future");
-        } else if (currHour === moment().hours()) {
+        } else if (parseInt($(this).attr("id")) === moment().hours()) {
             $(this).addClass("present");
         } else {
             $(this).addClass("past");
         }
     })
 };
-
+//added a function to store key value pairs of hour selected and input made
 function localStorageSave(){
 localStorage.setItem($(this).siblings(".hour").text(), $(this).siblings(".task").val());
 }
+//ebent listener to save button to trigger local storage function
 $(".saveBtn").on("click", localStorageSave);
-
+//function for when users refresh screen, the input remains if they've entered an input before through getting the input from local storage
 function enterTask() {
     $(".hour").each(function() {
         if(localStorage.getItem($(this).text()) !== null) {
@@ -25,5 +26,6 @@ function enterTask() {
         }
     });
 }
+//calling our functions
 timeColorChange();
 enterTask();
